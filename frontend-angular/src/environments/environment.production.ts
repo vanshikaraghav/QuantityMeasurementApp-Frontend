1de0263f88fc0819645c8253b2111b82b3f24ce2
vanshika raghav <vanshikathakur46@gmail.com>
2026-04-20 13:47:17 +0530

    [Vanshika Raghav] add. frontend

--- frontend-angular/src/environments/environment.production.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'REPLACE_WITH_YOUR_GATEWAY_URL'
-};
--- frontend-angular/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-// src/environments/environment.ts
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8080'  // API Gateway URL
-};
